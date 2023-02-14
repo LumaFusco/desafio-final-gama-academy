17567f043168c8f9b43cb13ae34cdf482e656d1b
Christhofer <christhoferxx@gmail.com>
2023-02-13 21:47:42 -0300

    Merge footer and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header";
-
+import Header from "./components/Header/index";
+import {Title} from './components/Title/title';
+import Footer from './components/footer/Footer'
 function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>Pagina Home</h1>
+      <Title titleText={"Aqui vamos inserir o título"} subTitle={'Aqui vamos inserir o subtitle (esse campo é opcional)'} />
+      <Footer />
     </div>
 
   );
