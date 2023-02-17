3b82701f214836a64e99587e297a21017751bb19
LumaFusco <lumafuscoconde@gmail.com>
2023-02-16 23:35:13 -0300

    inicial-commit-pagina-livro

---
+++ src/components/DescricaoLivro/livro.style.ts
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+
+margin-top:20px;
+
+h1 {
+       font-weight: 600;
+    font-size: 30px;
+    color: #009688;
+    }
+
+h3 {
+    font-weight: 200;
+    font-size: 16px;
+    color: #757575;
+    } 
+
+h4 {
+    font-weight: 200;
+    font-size: 15px;
+    color: #607D8B;
+    margin-bottom:20px;
+    } 
+
+h5 {
+    font-weight: 600;
+    font-size: 25px;
+    color: #212121;
+    } 
+
+    `;
---
+++ src/pages/Livro/livro.style.ts
@@ -0,0 +1,12 @@
+import  styled, {css}  from "styled-components";
+
+export const Container = styled.div`
+
+margin-top:20px;
+padding:5px;
+
+        .imageBook{
+        width:30%;
+        min-width: 250px;
+              }
+        `;
