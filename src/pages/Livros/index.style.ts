ce52c5a8e8bf656e7f727feac42bce27d9395ec6
Christhofer <christhoferxx@gmail.com>
2023-02-16 22:50:48 -0300

    starting the page Livros

---
+++ src/pages/Livros/index.style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    .context{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        height: 80vh;
+        max-width: 1320px;
+        padding: 10px; 
+        margin: 0 auto;
+        margin-top: 70px;
+        
+    }
+    h1{
+        color: #00796B;
+        font-weight: 600;
+        font-size: 36px;
+    }
+    h4{
+        font-size: 18px;
+        font-weight: 400;
+        max-width: 45%;
+        color: #979797;
+    }
+`;
--- src/pages/Livros/index.tsx
+++ src/pages/Livros/index.tsx
@@ -1,12 +1,26 @@
 import Header from '../../components/Header'
 import Footer from '../../components/footer/Footer'
+import * as C from './index.style'
+
+
+
 
 export default function Livros() {
   return (
     <div>
-      <Header />
-      <h1>Essa é a página dos LIVROS</h1>
-      <Footer />
+      <header>
+        <Header />
+      </header>
+      <C.Container>
+        <div className='context'>
+          <h1>Nossos Livros</h1>
+          <h4>Revamp your style with the latest designer trends in men’s clothing or achieve a perfectly curated wardrobe thanks to our line-up of timeless pieces. </h4>
+        </div>
+      </C.Container>
+        
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
