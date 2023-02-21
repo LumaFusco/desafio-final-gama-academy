3fecc8a09aaf268e12ee79c4cc9134d5443ff59f
Christhofer <christhoferxx@gmail.com>
2023-02-20 22:54:53 -0300

    search categorias

--- src/pages/Livros/index.tsx
+++ src/pages/Livros/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header'
 import Footer from '../../components/footer/Footer'
 import * as C from './index.style'
-
+import Button from '../../components/Button/';
 
 
 
@@ -15,7 +15,9 @@ export default function Livros() {
         <div className='context'>
           <h1>Nossos Livros</h1>
           <h4>Revamp your style with the latest designer trends in men’s clothing or achieve a perfectly curated wardrobe thanks to our line-up of timeless pieces. </h4>
+          <Button />
         </div>
+        
       </C.Container>
         
       <footer>
