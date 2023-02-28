8b257dea5e2a4380b0eb45313cd2e304ca535fd0
LumaFusco <lumafuscoconde@gmail.com>
2023-02-27 22:19:30 -0300

    pagina-sucesso-finalizada

---
+++ src/pages/Sucesso/sucesso.style.ts
@@ -0,0 +1,22 @@
+import  styled, {css}  from "styled-components";
+
+export const Container = styled.div`
+
+margin-top:20px;
+padding-left:50px;
+padding-right:50px;
+
+h6{
+    margin-top:15px;
+}
+
+.total{
+    margin-top:15px;
+    font-weight:bold;
+}
+
+.resumo-pedido{
+margin-top: 15px;
+}
+
+`;
